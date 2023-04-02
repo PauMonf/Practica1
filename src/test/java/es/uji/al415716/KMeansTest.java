091fc44dd7d324bca1fd2596cdea1dcd915b0697
Pau318 <Pau318@users.noreply.github.com>
2023-04-02 19:44:23 +0200

    test practica 2

--- src/test/java/es/uji/al415716/KMeansTest.java
+++ src/test/java/es/uji/al415716/KMeansTest.java
@@ -3,10 +3,11 @@ package es.uji.al415716;
 import es.uji.al415716.Algorithm.CSV;
 import es.uji.al415716.Algorithm.ExceptionKmeans;
 import es.uji.al415716.Algorithm.KMeans;
-import es.uji.al415716.Row.Row;
-import es.uji.al415716.Row.RowWithLabel;
+import es.uji.al415716.Table.Table;
 import es.uji.al415716.Table.TableWithLabels;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.FileNotFoundException;
 import java.util.List;
@@ -19,23 +20,27 @@ class KMeansTest {
 
     @Test
     void estimate() throws Exception {
-        CSV csv=new CSV();
+        CSV csv = new CSV();
         String sep = System.getProperty("file.separator");
-        TableWithLabels table=csv.readTableWithLabel("data"+sep+"basic1.csv");
-        KMeans kmeans=new KMeans(4,5,1);
+        TableWithLabels table = csv.readTableWithLabel("data" + sep + "basic1.csv");
+        KMeans kmeans = new KMeans(4, 5, 1);
         kmeans.train(table);
-        int x= kmeans.estimate(List.of(79.40828883263248,152.8344238267352));
-        System.out.println("Resultado: "+x+" Esperado: 0");
-        x= kmeans.estimate(List.of(262.35201641365506,64.57463208572932));
-        System.out.println("Resultado: "+x+" Esperado: 1");
 
-        int y;
-        RowWithLabel rowWithLabel;
-        for(Row row:table.getRows()){
-            x=kmeans.estimate(row.getData());
-            rowWithLabel=(RowWithLabel) row;
-            y=rowWithLabel.getNumberClass();
-            System.out.println("Resultado: "+x+" Esperado: "+y);
-        }
+        List<Double> a1 = List.of(96.5, 192.0);
+        List<Double> a2 = List.of(112.5, 222.0);
+        assertEquals(kmeans.estimate(a1), kmeans.estimate(a2));
+        List<Double> b1 = List.of(250.0,125.0);
+        List<Double> b2 = List.of(274.0,111.5);
+        assertEquals(kmeans.estimate(b1), kmeans.estimate(b2));
+        List<Double> c1 = List.of(360.0,360.0);
+        List<Double> c2 = List.of(380.0,320.0);
+        assertEquals(kmeans.estimate(c1), kmeans.estimate(c2));
+        List<Double> d1 = List.of(500.0,180.0);
+        List<Double> d2 = List.of(500.0,200.5);
+        assertEquals(kmeans.estimate(d1), kmeans.estimate(d2));
+
+        assertNotEquals(kmeans.estimate(a1), kmeans.estimate(b2));
+        assertNotEquals(kmeans.estimate(b1), kmeans.estimate(c2));
+        assertNotEquals(kmeans.estimate(c1), kmeans.estimate(d2));
     }
 }
